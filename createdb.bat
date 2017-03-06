31b5314cc4a0db89f2a2f9e2d8e8bc399bd03827
Stephane <stephane@sbnet.fr>
2017-03-06 15:19:53 +0100

    Areas management

--- createdb.bat
+++ createdb.bat
@@ -3,4 +3,5 @@ php bin/console doctrine:database:create
 php bin/console doctrine:generate:entities Sbnet
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load -n
- 
+
+# DELETE FROM city WHERE post_code IS NULL;
