083cf758296c626b51a29e2f01815134747bc095
Stephane BRUN <stephane@sbnet.fr>
2017-03-10 17:38:23 +0100

    Gentile

--- createdb.bat
+++ createdb.bat
@@ -1,5 +1,5 @@
 php bin/console doctrine:database:drop --force
 php bin/console doctrine:database:create
-php bin/console doctrine:generate:entities Sbnet
+php bin/console doctrine:generate:entities Sbnet --no-backup
 php bin/console doctrine:schema:update --force
 php bin/console doctrine:fixtures:load -n
